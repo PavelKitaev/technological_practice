850a4dc457d8775835ab95d815317be01d69137b
PavelKitaev <A359@live.ru>
2022-06-05 14:56:29 +0300

    init

---
+++ scripts/run.bat
@@ -0,0 +1,22 @@
+@echo off
+
+for /r "." %%a in (build\bin\*_seq.exe) do (
+    echo -------------------------------------
+    echo %%~na
+    echo -------------------------------------
+    %%~fa --gtest_repeat=10
+)
+
+for /r "." %%a in (build\bin\*_tbb.exe) do (
+    echo -------------------------------------
+    echo %%~na
+    echo -------------------------------------
+    %%~fa --gtest_repeat=10
+)
+
+for /r "." %%a in (build\bin\*_std.exe) do (
+    echo -------------------------------------
+    echo %%~na
+    echo -------------------------------------
+    %%~fa --gtest_repeat=10
+)
---
+++ scripts/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# shellcheck disable=SC2164
+cd build
+ctest --extra-verbose --repeat-until-fail 10 --timeout 1000 --build-and-test
+# shellcheck disable=SC2103
+cd ..
+
+FILES_SEQ="build/bin/*_seq"
+for file in $FILES_SEQ; do
+        echo "--------------------------------"
+        echo $(basename $file)
+        echo "--------------------------------"
+        valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./$file
+done
+
+# FILES_TBB="build/bin/*_tbb"
+# for file in $FILES_TBB; do
+#         echo "--------------------------------"
+#         echo $(basename $file)
+#         echo "--------------------------------"
+#         valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./$file
+# done
+
+FILES_STD="build/bin/*_std"
+for file in $FILES_STD; do
+        echo "--------------------------------"
+        echo $(basename $file)
+        echo "--------------------------------"
+        valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./$file
+done
